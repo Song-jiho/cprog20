6fd63125e7843f2c05cac0932208558ca211d910
Song-jiho <robot6573@naver.com>
2020-09-21 13:53:14 +0900

    overflow.c added

---
+++ overflow.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+	short s_money = SHRT_MAX; //최대값으로 초기화한다.
+	unsigned short u_money = USHRT_MAX; //최대값으로 초기화한다.
+	
+	s_money += 1;
+	printf("s_money = %d\n", s_money);
+
+	u_money += 1;
+	printf("u_money = %d\n", u_money);
+
+	return 0;
+}
